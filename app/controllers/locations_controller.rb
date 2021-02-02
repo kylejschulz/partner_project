b0103ed70529e4ff01ffdb2c54e668e6c09da2ff
Arika Turing <ablenker@gmail.com>
2021-02-02 15:41:16 -0700

    Create intital add location feature

---
+++ app/controllers/locations_controller.rb
@@ -0,0 +1,9 @@
+class LocationsController < ApplicationController
+  def index
+    @locations = ['Salt Lake City', 'Denver']
+  end
+
+  def new
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/', to: 'climbing_gym#index'
+  get '/locations', to: 'locations#index'
+  get '/locations/new', to: 'locations#new'
+  get '/locations', to: 'locations#create'
 end

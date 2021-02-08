a47c07a3cec4b47648ead86ef3a963c5c17b7e7f
Arika Turing <ablenker@gmail.com>
2021-02-07 18:47:56 -0700

    Finish user stories 22 and 23

--- spec/features/locations/index_spec.rb
+++ spec/features/locations/index_spec.rb
@@ -2,17 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'locations index page', type: :feature do
   it 'see the name of each parent record in the system' do
-    location_1 = Location.create!(city: 'Salt Lake City', square_footage: 40000, lead_wall: true)
-    location_2 = Location.create!(city: 'Seattle', square_footage: 35000, lead_wall: false)
+    location_1 = Location.create(city: 'Salt Lake City',
+                                 square_footage: 40000,
+                                 lead_wall: true)
+    location_2 = Location.create(city: 'Seattle',
+                                 square_footage: 35000,
+                                 lead_wall: false)
 
     visit '/locations'
 
     expect(page).to have_content(location_1.city)
     expect(page).to have_content(location_2.city)
-    expect(page).to have_content(location_1.square_footage)
-    expect(page).to have_content(location_2.square_footage)
-    expect(page).to have_content(location_1.lead_wall)
-    expect(page).to have_content(location_2.lead_wall)
+    # expect(page).to have_content(location_1.square_footage)
+    # expect(page).to have_content(location_2.square_footage)
+    # expect(page).to have_content(location_1.lead_wall)
+    # expect(page).to have_content(location_2.lead_wall)
   end
 
+  # it 'follow a link to ceate new location' do
+  #   visit '/locations'
+  #
+  #   expect(page).to have
 end
